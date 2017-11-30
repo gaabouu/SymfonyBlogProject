de395b51e6e8b159a0b41a8046966eba6cbbb4d9
glarbalete <gabriel.larbalete@gmail.com>
2017-11-30 13:15:50 +0100

    initializing proect

---
+++ README.md
@@ -0,0 +1,4 @@
+zeBlog
+======
+
+A Symfony project created on November 23, 2017, 3:07 pm.
---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/post/{idPost}", name="Post", requirements={"idPost" : "\d+" })
+     */
+    public function postAction($idPost)
+    {
+        return $this->render('default/post.html.twig', ['idPost'=>$idPost]);
+    }
+
+    /**
+     * @Route("/post/{idPost}", name="wrong_post")
+     */
+    public function wrongPostAction($idPost)
+    {
+        return new Response('<h1>L\'identifiant ' . $idPost . ' ne correspond à aucun post</h1>');
+    }
+}
+
